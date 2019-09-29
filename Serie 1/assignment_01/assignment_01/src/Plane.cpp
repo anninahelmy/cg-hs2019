d2adaad114551ad68713ff84fefbdab809124bd6
joana <joana.portmann@students.unibe.ch>
2019-09-29 13:20:43 +0200

    Implement intersection funktion for plane

--- Serie 1/assignment_01/assignment_01/src/Plane.cpp
+++ Serie 1/assignment_01/assignment_01/src/Plane.cpp
@@ -20,7 +20,7 @@
 
 
 Plane::Plane(const vec3& _center, const vec3& _normal)
-: center(_center), normal(_normal)
+	: center(_center), normal(_normal)
 {
 }
 
@@ -31,19 +31,19 @@ Plane::Plane(const vec3& _center, const vec3& _normal)
 bool
 Plane::
 intersect(const Ray& _ray,
-          vec3&      _intersection_point,
-          vec3&      _intersection_normal,
-          double&    _intersection_t ) const
+	vec3& _intersection_point,
+	vec3& _intersection_normal,
+	double& _intersection_t) const
 {
 
-/** \todo
- * - compute the intersection of the plane with `_ray`
- * - if ray and plane are parallel there is no intersection
- * - otherwise compute intersection data and store it in `_intersection_point`, `_intersection_normal`, and `_intersection_t`.
- * - return whether there is an intersection in front of the viewer (t > 0)
-*/
+	/** \todo
+	 * - compute the intersection of the plane with `_ray`
+	 * - if ray and plane are parallel there is no intersection
+	 * - otherwise compute intersection data and store it in `_intersection_point`, `_intersection_normal`, and `_intersection_t`.
+	 * - return whether there is an intersection in front of the viewer (t > 0)
+	*/
 
-    return false;
+	return false;
 }
 
 
