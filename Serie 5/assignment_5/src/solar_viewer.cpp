0152b3693dbdfc3dc99a57ce8efd6bbb542c8d5d
joana <joana.portmann@students.unibe.ch>
2019-10-27 02:36:11 +0200

    place and rotate mercury and moon

--- Serie 5/assignment_5/src/solar_viewer.cpp
+++ Serie 5/assignment_5/src/solar_viewer.cpp
@@ -209,8 +209,11 @@ void Solar_viewer::update_body_positions() {
      *       is fixed for now.
      * */
 
-	earth_.pos_ = sun_.pos_ + vec4(earth_.distance_, 0, 0, 0);
-	earth_.time_step(1);
+	earth_.pos_ = mat4::rotate_y(earth_.angle_orbit_) * (sun_.pos_ + vec4(earth_.distance_, 0, 0, 0));
+
+	mercury_.pos_ = mat4::rotate_y(mercury_.angle_orbit_) * (sun_.pos_ + vec4(mercury_.distance_, 0, 0, 0));
+
+	moon_.pos_ = earth_.pos_ + mat4::rotate_y(moon_.angle_orbit_) * vec4(moon_.distance_, 0, 0, 0);
 }
 
 //-----------------------------------------------------------------------------
@@ -401,7 +404,7 @@ void Solar_viewer::draw_scene(mat4& _projection, mat4& _view)
     unit_sphere_.draw();
 
 	// render earth
-	m_matrix = mat4::rotate_y(earth_.angle_orbit_) * mat4::translate(earth_.pos_) * mat4::rotate_y(earth_.angle_self_) * mat4::scale(earth_.radius_);
+	m_matrix = mat4::translate(earth_.pos_) * mat4::rotate_y(earth_.angle_self_) * mat4::scale(earth_.radius_);
 	mv_matrix = _view * m_matrix;
 	mvp_matrix = _projection * mv_matrix;
 	color_shader_.use();
@@ -410,6 +413,30 @@ void Solar_viewer::draw_scene(mat4& _projection, mat4& _view)
 	color_shader_.set_uniform("tex", 0);
 	color_shader_.set_uniform("greyscale", (int)greyscale_);
 	earth_.tex_.bind();
+	unit_sphere_.draw();
+
+	// render mercury
+	m_matrix = mat4::translate(mercury_.pos_) * mat4::rotate_y(mercury_.angle_self_) * mat4::scale(mercury_.radius_);
+	mv_matrix = _view * m_matrix;
+	mvp_matrix = _projection * mv_matrix;
+	color_shader_.use();
+	color_shader_.set_uniform("modelview_projection_matrix", mvp_matrix);
+
+	color_shader_.set_uniform("tex", 0);
+	color_shader_.set_uniform("greyscale", (int)greyscale_);
+	earth_.tex_.bind();
+	unit_sphere_.draw();
+
+	// render moon
+	m_matrix = mat4::translate(moon_.pos_) * mat4::rotate_y(moon_.angle_self_) * mat4::scale(moon_.radius_);
+	mv_matrix = _view * m_matrix;
+	mvp_matrix = _projection * mv_matrix;
+	color_shader_.use();
+	color_shader_.set_uniform("modelview_projection_matrix", mvp_matrix);
+
+	color_shader_.set_uniform("tex", 0);
+	color_shader_.set_uniform("greyscale", (int)greyscale_);
+	moon_.tex_.bind();
 	unit_sphere_.draw();
 
 	
