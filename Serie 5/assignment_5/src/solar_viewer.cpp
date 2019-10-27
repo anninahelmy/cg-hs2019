856b4e465be4ecdfbbfc154eeb14aba74dd5c01c
Ann Kiener <ann_kiener@hotmail.com>
2019-10-27 11:25:38 +0100

    add stars background

--- Serie 5/assignment_5/src/solar_viewer.cpp
+++ Serie 5/assignment_5/src/solar_viewer.cpp
@@ -467,6 +467,17 @@ void Solar_viewer::draw_scene(mat4& _projection, mat4& _view)
 	mars_.tex_.bind();
 	unit_sphere_.draw();
 
+	// render stars
+	m_matrix = mat4::translate(stars_.pos_) * mat4::rotate_y(stars_.angle_self_) * mat4::scale(stars_.radius_);
+	mv_matrix = _view * m_matrix;
+	mvp_matrix = _projection * mv_matrix;
+	color_shader_.use();
+	color_shader_.set_uniform("modelview_projection_matrix", mvp_matrix);
+
+	color_shader_.set_uniform("tex", 0);
+	color_shader_.set_uniform("greyscale", (int)greyscale_);
+	stars_.tex_.bind();
+	unit_sphere_.draw();
 	
     /** \todo Render the star background, the spaceship, and the rest of the celestial bodies.
      *  For now, everything should be rendered with the color_shader_,
