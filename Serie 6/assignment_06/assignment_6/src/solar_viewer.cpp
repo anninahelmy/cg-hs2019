682705116f384cbcfba8d6658aa55ccd5dadbada
Ann Kiener <ann_kiener@hotmail.com>
2019-11-04 16:51:27 +0100

    add textures, change ships shader

--- Serie 6/assignment_06/assignment_6/src/solar_viewer.cpp
+++ Serie 6/assignment_06/assignment_6/src/solar_viewer.cpp
@@ -439,11 +439,12 @@ void Solar_viewer::draw_scene(mat4& _projection, mat4& _view)
 	m_matrix = mat4::translate(ship_.pos_) * mat4::rotate_y(ship_.angle_) * mat4::scale(ship_.radius_);
 	mv_matrix = _view * m_matrix;
 	mvp_matrix = _projection * mv_matrix;
-	color_shader_.use();
-	color_shader_.set_uniform("modelview_projection_matrix", mvp_matrix);
-
-	color_shader_.set_uniform("tex", 0);
-	color_shader_.set_uniform("greyscale", (int)greyscale_);
+	phong_shader_.use();
+	phong_shader_.set_uniform("modelview_projection_matrix", mvp_matrix);
+	phong_shader_.set_uniform("modelview_matrix", mv_matrix);
+	phong_shader_.set_uniform("normal_matrix", mat3(transpose(inverse(mv_matrix))));
+	phong_shader_.set_uniform("tex", 0);
+	phong_shader_.set_uniform("greyscale", (int)greyscale_);
 	ship_.tex_.bind();
 	ship_.draw();
 
@@ -499,7 +500,10 @@ void Solar_viewer::draw_scene(mat4& _projection, mat4& _view)
 	earth_shader_.set_uniform("modelview_matrix", mv_matrix);
 	earth_shader_.set_uniform("normal_matrix", normal_matrix);
 	earth_shader_.set_uniform("light_position", _view* vec4(0, 0, 0, 1));
-
+	earth_shader_.set_uniform("day_texture", 0);
+    earth_shader_.set_uniform("night_texture", 1);
+    earth_shader_.set_uniform("cloud_texture", 2);
+    earth_shader_.set_uniform("gloss_texture", 3);
 	earth_shader_.set_uniform("tex", 0);
 	earth_shader_.set_uniform("greyscale", (int)greyscale_);
 	earth_.tex_.bind();
