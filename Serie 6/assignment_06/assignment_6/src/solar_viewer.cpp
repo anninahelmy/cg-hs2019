b234063250bb3495e5f9f843288281d6ce0614b3
joana <joana.portmann@students.unibe.ch>
2019-11-04 14:50:56 +0100

    add earth shader

--- Serie 6/assignment_06/assignment_6/src/solar_viewer.cpp
+++ Serie 6/assignment_06/assignment_6/src/solar_viewer.cpp
@@ -487,6 +487,26 @@ void Solar_viewer::draw_scene(mat4& _projection, mat4& _view)
 	sunglow_.draw();
 
 
+	// Render earth
+	m_matrix = mat4::translate(earth_.pos_) * mat4::rotate_y(earth_.angle_self_) * mat4::scale(earth_.radius_);
+	mv_matrix = _view * m_matrix;
+	mvp_matrix = _projection * mv_matrix;
+
+	mat3 normal_matrix = transpose(inverse(mat3(mv_matrix)));
+
+	earth_shader_.use();
+	earth_shader_.set_uniform("modelview_projection_matrix", mvp_matrix);
+	earth_shader_.set_uniform("modelview_matrix", mv_matrix);
+	earth_shader_.set_uniform("normal_matrix", normal_matrix);
+	earth_shader_.set_uniform("light_position", _view* vec4(0, 0, 0, 1));
+
+	earth_shader_.set_uniform("tex", 0);
+	earth_shader_.set_uniform("greyscale", (int)greyscale_);
+	earth_.tex_.bind();
+	unit_sphere_.draw();
+
+
+
     // check for OpenGL errors
     glCheckError();
 }
